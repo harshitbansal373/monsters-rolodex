87429eefa848f63da4da4a182e5f328bd79fe016
harshitbansal373 <harshitbansal373@gmail.com>
2020-07-19 17:19:21 +0530

    Updates

---
+++ precache-manifest.99f22a32c84bfc8d590872d0fe332c7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbbc962a5e037d35c527a527abb9201",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "25d9fa6284d4d174d357",
+    "url": "/monsters-rolodex/static/css/main.29179632.chunk.css"
+  },
+  {
+    "revision": "2c643acc1b1026722189",
+    "url": "/monsters-rolodex/static/js/2.f9462cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.f9462cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d9fa6284d4d174d357",
+    "url": "/monsters-rolodex/static/js/main.0c9e5b96.chunk.js"
+  },
+  {
+    "revision": "d1f275d9f5077745035c",
+    "url": "/monsters-rolodex/static/js/runtime-main.1c12e2cf.js"
+  }
+]);
